4cd60d7e9db061940c5a13e248529488261fe19f
yevine <166761864+yevine@users.noreply.github.com>
2024-06-17 21:53:16 -0400

    Create concepts.sh

---
+++ Baics/concepts.sh
@@ -0,0 +1,30 @@
+Echo is a Unix/Linux command tool used for displaying lines of text or string which are passed as arguments on the command line.
+ way to comment in Bash
+ using : ‘ operator.
+
+: '
+Code or Comment here
+ '
+  using : <<
+ <<comment
+ "Code" or "Comments"
+comment
+
+if [condition];then
+  cmds
+else
+ cmds
+fi
+
+if [condition];then
+  cmds
+elif
+ cmds
+fi
+### os name cmd, UNAME for--- linux & mac  VER --Windows
+
+if [condition];then
+  cmds
+elif
+ cmds
+fi
